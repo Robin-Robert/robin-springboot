878adfb4c5c00e5f233c6e51179c30e5fa4cfc46
Robin-Robert <sishencoming@sina.com>
2017-12-27 00:02:22 +0800

    1227

--- src/main/java/cn/robin/springboot/controller/HelloController.java
+++ src/main/java/cn/robin/springboot/controller/HelloController.java
@@ -1,15 +1,23 @@
 package cn.robin.springboot.controller;
 
+
+
+
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HelloController {
 
-	@RequestMapping("hello")
-	@ResponseBody
-	public String hello() {
-		return "this is my frist springboot page";
+	private final Logger log = Logger.getLogger(this.getClass());
+	
+	@RequestMapping(value = "/hello",method = RequestMethod.GET)
+	public String hello(Model model) {
+		log.info("log 有了");
+		 model.addAttribute("name", "Dear");
+         return "hello";
 	}
 }
